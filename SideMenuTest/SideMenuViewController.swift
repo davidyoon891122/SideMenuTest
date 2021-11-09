3409f5ca843e534e1142b13f4dc196f90dd9f84a
davidyoon <qqpo12@naver.com>
2021-11-09 12:10:48 +0900

    add testButton on MainViewController

--- SideMenuTest/SideMenuViewController.swift
+++ SideMenuTest/SideMenuViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class SideMenuViewController: UIViewController {
+class SideMenuViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
--- SideMenuTest/ViewController.swift
+++ SideMenuTest/ViewController.swift
@@ -10,13 +10,29 @@ import UIKit
 class ViewController: UIViewController {
     private var sideMenuViewControll: SideMenuViewController!
     
+    private let testButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("TEST", for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        addSubviews()
+        setLayoutConstrains()
     }
 
 
+    
+    private func addSubviews() {
+        view.addSubview(testButton)
+    }
+    
+    private func setLayoutConstrains() {
+        testButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        testButton.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+    }
 }
 
